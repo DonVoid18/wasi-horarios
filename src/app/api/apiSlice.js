600385ebf8650fd1891917e24a628d01b2a23777
patrick rios <angelopatrickriosnolasco@gmail.com>
2023-07-21 16:47:43 -0500

    feat: change url backend

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -4,7 +4,7 @@ import { setCredentials } from "../../features/auth/authSlice";
 const baseQuery = fetchBaseQuery({
   // develop
   // baseUrl: "http://localhost:3000",
-  baseUrl: "https://api-WASI.onrender.com",
+  baseUrl: "https://api-unilix.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
